5e162b867ca2f9e805ed7257c62d8cf7099a87c9
worflor <126105721+worflor@users.noreply.github.com>
2023-09-19 23:52:02 -0400

    Weapons.h added
    it has 4 classes within the file.
    1. Weapon
    2. Melee
    3. Ranged
    4. Shield
    
    - everything is in the header file for now.
    - constructors done
    - methods in place
    - variables there
    - inHeRiTancE (wow!)
    
    *MISSING MAJOR FUNCTIONALITY*
    - no sdl integration cuz its scary
    - no projectile class
    - no projectiles
    - no math yet
    - needs therapy
    - needs to be separated later

---
+++ SDL_Framework23.0.0/Framework/Weapons.cpp
@@ -0,0 +1,2 @@
+#include "Weapons.h"
+//so empty
---
+++ SDL_Framework23.0.0/Framework/Weapons.h
@@ -0,0 +1,197 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+
+///	Weapons Parent Class
+
+
+class Weapon {
+protected:
+	//	weapon's name
+	std::string name;
+	//	amount of damage dealt
+	int damage;
+	//	range of the weapon
+	int range;
+
+public:
+
+	Weapon(std::string weaponName, int damage, int range) : 
+		name(weaponName), 
+		damage(damage), 
+		range(range) 
+	{ }
+
+	Weapon() {
+		name = "unknown weapon";
+		damage = 0;
+		range = 0;
+	}
+
+	virtual ~Weapon() { };
+
+	virtual std::string getName() const { return name; }
+	virtual int getDamage() const { return damage; }
+	virtual int getRange() const { return range;	}
+
+	virtual void attack();
+};
+
+
+///	Melee Weapons
+
+
+class Melee : public Weapon {
+protected:
+	float sharpness;
+	int swingSpeed;
+	int staminaCost;
+
+public:
+	//name, damage, range, sharpness(0-1), swing speed, stamina cost(max stamina could be 100)
+	Melee(std::string weaponName, int damage, int range, int sharpness, int swingSpeed, int staminaCost) :
+		Weapon(weaponName, damage, range),
+		sharpness(sharpness),
+		swingSpeed(swingSpeed),
+		staminaCost(staminaCost)
+	{ };
+
+	Melee() {
+		name = "unknown melee weapon";
+		damage = 0;
+		range = 0;
+		sharpness = 0.0f;
+		swingSpeed = 0;
+		staminaCost = 0;
+	}
+
+	~Melee() {}
+
+	int getSharpness() const { return sharpness; }
+	int getSwingSpeed() const {	return swingSpeed; }
+	int getStaminaCost() const { return staminaCost; }
+	int getDamage() {	
+		float damageModifier = sharpness + 1; //	sharpness should always be between 0 and 1 (or close to that)
+		return damage * damageModifier;
+		}
+
+	void attack() override {
+		bool
+			swingCon = true;
+
+		if (swingCon) {
+			swing();
+		} else {
+			std::cout << "n\n";
+		}
+	}
+
+	void swing() {
+		staminaCost = 25;
+		getDamage();
+	}
+
+	void slash() {
+		staminaCost = 15;
+		getDamage();
+	}
+
+	void stab() {
+		staminaCost = 10;
+		getDamage();
+	}
+};
+
+///	Ranged Weapons
+
+class Ranged : public Weapon {
+protected:
+	int ammo;
+	int reloadTime;
+	
+public:
+	
+	Ranged(std::string weaponName, int damage, int range, int ammo, int reloadTime) : 
+		Weapon(weaponName, damage, range), 
+		ammo(ammo), 
+		reloadTime(reloadTime) 
+	{ };
+
+	Ranged() {
+		name = "unknown ranged weapon";
+		damage = 0;
+		range = 0;
+		ammo = 0;
+		reloadTime = 0;
+	}
+
+	~Ranged() {}
+
+	int getAmmo() const { return ammo; }
+	int getReloadTime() const {	return reloadTime; }
+
+	void attack() override;
+
+	void reload();
+};
+
+///	Shields
+
+class Shield : public Weapon {
+protected:
+	int durability; //	100
+	int maxDurability; // 100
+	float blockChance;
+	int blockPower;
+
+public:
+
+	Shield(std::string weaponName, int damage, int range, int durability, int maxDurability, float blockChance, int blockPower) :
+		Weapon(weaponName, damage, range),
+		durability(durability),
+		maxDurability(maxDurability),
+		blockChance(blockChance),
+		blockPower(blockPower) 
+	{ };
+
+	Shield() {
+		name = "unknown shield";
+		damage = 0;
+		range = 0;
+		durability = 0;
+		maxDurability = 0;
+		blockChance = 0;
+		blockPower = 0;
+	}
+
+	~Shield() {}
+
+	int getDurability() const { return durability; }
+	int getMaxDurability() const { return maxDurability; }
+	float getBlockChance() const { return blockChance; }
+	int getBlockPower() const { return blockPower; }
+
+	//TO DO
+	void attack() override;
+
+	//	returns how much damage is blocked by the current shield
+	int getDamage(int incomingDamage) {
+		//to do: add blocking logic
+		bool doesBlock = true;
+
+		if (doesBlock) {
+			return incomingDamage * blockPower;
+		} else {
+			return incomingDamage;
+		}
+
+
+	};
+
+	void bash();
+
+	void damageAura();
+
+	void repair() { durability = maxDurability; };
+};
